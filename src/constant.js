ec01027c9154a337fca608d5b8def6817aa44aaa
Arif <snakepliskinz@gmail.com>
2017-01-13 15:18:39 +0700

    Hide the firebase credential

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-export const API_KEY = 'AIzaSyANx9iqyKSdbG4Uu4CRCrR6gGamXj9GK5s';
-export const AUTH_DOMAIN = "wemos-arduino.firebaseapp.com";
-export const DATABASE_URL = "https://wemos-arduino.firebaseio.com";
-export const STORAGE_BUCKET = "wemos-arduino.appspot.com";
-export const MESSAGING_SENDER_ID = "345464825282";
+export const API_KEY = '***';
+export const AUTH_DOMAIN = "***";
+export const DATABASE_URL = "***";
+export const STORAGE_BUCKET = "***";
+export const MESSAGING_SENDER_ID = "***";
